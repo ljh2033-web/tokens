41cea4ccf7ff81a1e746f5859470053aced75a89
ljh2033-web <ljh2033@126.com>
2025-12-31 20:23:54 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,8 @@ async function c1(){
     await navigator.clipboard.writeText("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");
 }
 async function c2(){
-    alert("tokens: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")
-    await navigator.clipboard.writeText("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")
+    alert("tokens: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");
+    await navigator.clipboard.writeText("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");
 }
 c1_tk.addEventListener("click",c1);
 c2_tk.addEventListener("click",c2);
