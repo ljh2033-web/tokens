0fa16b1ea7ddb45ebe7bb951437684b0f2268b4e
ljh2033-web <ljh2033@126.com>
2025-12-31 19:53:51 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -8,4 +8,29 @@ async function c1() {
         console.error('Failed to copy: ', err);
     }
 }
-c1_tk.addEventListener("click",c1)
+c2_tk.addEventListener("click",c2)
+const c2_tk = document.getElementById("c2")
+async function c2() {
+    try {
+        let user_check = confirm("这个秘钥很重要！你确定要吗？")
+        if(user_check){
+            let user_check2 = confirm("真的?")
+            if(user_check2){
+                await navigator.clipboard.writeText('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\n');
+                console.log('Text copied to clipboard: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\n');
+            }
+            else {
+                alert("明智之举！");
+            }
+        else {
+            alert("太明智了");
+        }
+        }
+        await navigator.clipboard.writeText('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\n');
+        console.log('Text copied to clipboard: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\n');
+    }
+    catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
+c1_tk.addEventListener("click",c2)
