b1240b3fe1998ce813fadb0ab34c66860698f88a
ljh2033-web <ljh2033@126.com>
2025-12-31 20:21:13 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 let c1_tk = document.querySelector("c1");
-let c2_tk = document.querySelector("c2")
+let c2_tk = document.querySelector("c2");
 async function c1(){
     alert("tokens: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\n");
     await navigator.clipboard.writeText("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");
